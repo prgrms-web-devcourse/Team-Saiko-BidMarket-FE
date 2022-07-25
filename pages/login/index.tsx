679dd56facc1933cd0c24fffbd7223b951dbd308
wogha95 <wogha95@naver.com>
2022-07-26 03:21:44 +0900

    ✨ 로그인 화면에 Header 추가

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,10 @@
+import { Text } from '@chakra-ui/react';
+import Header from '@common/header';
+import GoBackIcon from '@common/header/goBackIcon';
 import type { NextPage } from 'next';
 
 const Login: NextPage = () => {
-  return <div>Login</div>;
+  return <Header left={<GoBackIcon />} middle={<Text>비드마켓</Text>} />;
 };
 
 export default Login;
