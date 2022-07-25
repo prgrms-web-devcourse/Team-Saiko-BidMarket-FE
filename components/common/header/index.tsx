65ddd3540c654106bbc1c66cabe86eb2a699d77b
wogha95 <wogha95@naver.com>
2022-07-26 02:03:46 +0900

    🎨 HeaderProp 작성

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,25 +1,22 @@
-import { ChevronLeftIcon, HamburgerIcon } from '@chakra-ui/icons';
-import { Center, Flex, Text } from '@chakra-ui/react';
+import { Center, Flex } from '@chakra-ui/react';
 
-const Header = ({
-  left,
-  middle,
-  right,
-}: {
-  left?: string;
-  middle?: string;
-  right?: string;
-}) => {
+interface HeaderProp {
+  left?: React.ReactNode;
+  middle?: React.ReactNode;
+  right?: React.ReactNode;
+}
+
+const Header = ({ left, middle, right }: HeaderProp) => {
   return (
     <Flex padding="10px" justifyContent="space-between" width="100%">
       <Flex alignItems="center" flex="1">
-        <ChevronLeftIcon boxSize="8" />
+        {left}
       </Flex>
       <Center alignItems="center" flex="1">
-        <Text>bidmarket</Text>
+        {middle}
       </Center>
       <Flex justifyContent="flex-end" alignItems="center" flex="1">
-        <HamburgerIcon boxSize="6" />
+        {right}
       </Flex>
     </Flex>
   );
