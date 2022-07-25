9a32a0536da7cedfa4ae52d09f2d249692382781
wogha95 <wogha95@naver.com>
2022-07-25 21:58:35 +0900

    🔥 불필요한 코드 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
