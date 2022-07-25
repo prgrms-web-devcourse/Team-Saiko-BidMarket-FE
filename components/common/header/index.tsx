db40f17a371cf862d18d6216291c811e291f2c5a
wogha95 <wogha95@naver.com>
2022-07-26 00:42:36 +0900

    💄 Header CSS 적용

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,7 +1,28 @@
-import type { NextPage } from 'next';
+import { ChevronLeftIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { Center, Flex, Text } from '@chakra-ui/react';
 
-const Header: NextPage = () => {
-  return <div>Header</div>;
+const Header = ({
+  left,
+  middle,
+  right,
+}: {
+  left?: string;
+  middle?: string;
+  right?: string;
+}) => {
+  return (
+    <Flex padding="10px" justifyContent="space-between" width="100%">
+      <Flex alignItems="center" flex="1">
+        <ChevronLeftIcon boxSize="8" />
+      </Flex>
+      <Center alignItems="center" flex="1">
+        <Text>bidmarket</Text>
+      </Center>
+      <Flex justifyContent="flex-end" alignItems="center" flex="1">
+        <HamburgerIcon boxSize="6" />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Header;
