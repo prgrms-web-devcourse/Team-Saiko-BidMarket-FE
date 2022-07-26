393b241ef2f59aeda4e519c3b15ea19b23d2f1a8
waterglasses <hsk10271@gmail.com>
2022-07-27 00:48:10 +0900

    :sparkles: Banner 컴포넌트 구현

---
+++ components/Main/Banner/index.tsx
@@ -0,0 +1,24 @@
+import { Flex, Box, Image, Text } from '@chakra-ui/react';
+
+const Banner = () => {
+  //TODO: 슬로건 수정하기
+  return (
+    <Flex justifyContent="space-between" alignItems="flex-end" marginTop="15px">
+      <Flex direction="column" gap="6px">
+        <Text fontSize="lg">
+          <Text as="span" color="#FF4370">
+            비딩
+          </Text>
+          을 통한
+        </Text>
+        <Text fontSize="lg">현명한 중고 거래</Text>
+        <Text fontSize="sm" color="#666666">
+          원하는 가격에 팔고, 좋은 물건은 싸게 산다!
+        </Text>
+      </Flex>
+      <Image src="/Banner/banner.png" alt="banner" height="65" />
+    </Flex>
+  );
+};
+
+export default Banner;
