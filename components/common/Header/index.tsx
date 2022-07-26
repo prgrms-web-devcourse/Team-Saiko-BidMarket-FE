ffe55bd295feaa51070e391207bf7ec1fc08cd66
wogha95 <wogha95@naver.com>
2022-07-27 08:36:35 +0900

    🎨 props type명 변경

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -1,12 +1,12 @@
 import { Center, Flex } from '@chakra-ui/react';
 
-interface HeaderProp {
+interface HeaderProps {
   leftContent?: React.ReactNode;
   middleContent?: React.ReactNode;
   rightContent?: React.ReactNode;
 }
 
-const Header = ({ leftContent, middleContent, rightContent }: HeaderProp) => {
+const Header = ({ leftContent, middleContent, rightContent }: HeaderProps) => {
   return (
     <Flex padding="10px" justifyContent="space-between" width="100%">
       <Flex alignItems="center" flex="1">
