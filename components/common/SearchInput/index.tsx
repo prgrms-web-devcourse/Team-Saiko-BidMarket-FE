2e9adb7f6125bfc072e774ec5ef2a714b37fb80e
waterglasses <hsk10271@gmail.com>
2022-07-27 01:23:57 +0900

    :truck: SearchInput 컴포넌트 위치 common 안으로 변경

--- components/common/SearchInput/index.tsx
+++ components/common/SearchInput/index.tsx
@@ -4,10 +4,9 @@ import { SearchIcon } from '@chakra-ui/icons';
 const SearchInput = () => {
   return (
     <InputGroup marginTop="15px">
-      <InputLeftElement
-        pointerEvents="none"
-        children={<SearchIcon color="#FF4370" />}
-      />
+      <InputLeftElement pointerEvents="none">
+        <SearchIcon color="#FF4370" />
+      </InputLeftElement>
       <Input
         type="text"
         placeholder="찾으시는 상품을 검색해보세요!"
