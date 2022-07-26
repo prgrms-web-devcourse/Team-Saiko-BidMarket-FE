c777184f19c1d23c26c4b7c8229ac63b6783bf95
wogha95 <wogha95@naver.com>
2022-07-26 16:00:47 +0900

    ✨ 로그인 화면에 페이지 제목 태그 추가

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,10 +1,16 @@
 import { Text } from '@chakra-ui/react';
 import Header from '@common/header';
 import GoBackIcon from '@common/header/goBackIcon';
+import Seo from '@common/seo';
 import type { NextPage } from 'next';
 
 const Login: NextPage = () => {
-  return <Header left={<GoBackIcon />} middle={<Text>비드마켓</Text>} />;
+  return (
+    <>
+      <Seo title="로그인" />
+      <Header left={<GoBackIcon />} middle={<Text>비드마켓</Text>} />
+    </>
+  );
 };
 
 export default Login;
