bfbb80ba08148470929182dbfc7ca63b1de65ae3
waterglasses <hsk10271@gmail.com>
2022-07-27 17:39:53 +0900

    :sparkles: 상품 상세 이미지 컴포넌트 구현

---
+++ components/ProductDetail/ProductImage.tsx
@@ -0,0 +1,9 @@
+import { Image } from '@chakra-ui/react';
+
+const ProductImage = () => {
+  return (
+    <Image width="100%" alt="product-image" src="https://bit.ly/code-beast" />
+  );
+};
+
+export default ProductImage;
