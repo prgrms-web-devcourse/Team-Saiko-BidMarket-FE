69bea08474930d4e230416c9247367b9a40a99fd
waterglasses <hsk10271@gmail.com>
2022-07-28 15:33:45 +0900

    :truck: Main 페이지 컴포넌트 폴더 구조 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
+import { Box, Divider, Flex } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import { Fragment } from 'react';
 
-import Main from 'components/Main';
+import Card from '@common/Card';
+import SearchInput from '@common/SearchInput';
+import { Banner, MainHeader, ProductAddButton } from 'components/Main';
 
 const Home: NextPage = () => {
-  return <Main />;
+  return (
+    <>
+      <MainHeader />
+      <Flex direction="column" width="100%">
+        <Banner />
+        <Divider marginTop="15px" />
+        <SearchInput />
+        {Array(10)
+          .fill(1)
+          .map((_, index) => {
+            return (
+              <Fragment key={index}>
+                <Card productId={index.toString()} />
+                <Divider />
+              </Fragment>
+            );
+          })}
+      </Flex>
+      <Box alignSelf="flex-end" position="sticky" bottom="15px" right="15px">
+        <ProductAddButton />
+      </Box>
+    </>
+  );
 };
 
 export default Home;
