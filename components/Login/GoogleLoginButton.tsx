6b7d6b0482fe51e73f9b268f6196a5eaeb552b3b
wogha95 <wogha95@naver.com>
2022-07-28 21:56:59 +0900

    ✨ 회원가입 페이지로 라우팅 적용

--- components/Login/GoogleLoginButton.tsx
+++ components/Login/GoogleLoginButton.tsx
@@ -1,12 +1,25 @@
 import { Box } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import Script from 'next/script';
+import { useRef } from 'react';
 
 const GoogleLoginButton = () => {
+  const router = useRouter();
+  const loginButton = useRef<HTMLDivElement>(null);
+
+  const handleLoginButtonClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation;
+    router.push(
+      'http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:3000/oauth/redirect'
+    );
+  };
+
   return (
     <>
       <Script src="https://accounts.google.com/gsi/client" async defer />
-      <Box id="g_id_onload" data-ux_mode="redirect" />
       <Box
+        id="g_id_onload"
+        data-ux_mode="redirect"
         className="g_id_signin"
         data-type="standard"
         data-size="large"
@@ -14,6 +27,8 @@ const GoogleLoginButton = () => {
         data-text="sign_in_with"
         data-shape="rectangular"
         data-logo_alignment="left"
+        ref={loginButton}
+        onClickCapture={handleLoginButtonClick}
       />
     </>
   );
