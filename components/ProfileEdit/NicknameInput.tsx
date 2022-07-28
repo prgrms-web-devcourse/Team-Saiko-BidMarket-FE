5bec36e1f24284d5218bc006398c4dfea0e67daa
Hoseok Na <hsna7024@gmail.com>
2022-07-28 18:27:58 +0900

    :recycle: input text를 nickname에 저장

--- components/ProfileEdit/NicknameInput.tsx
+++ components/ProfileEdit/NicknameInput.tsx
@@ -1,28 +1,40 @@
 import { EditIcon } from '@chakra-ui/icons';
 import { Flex, InputGroup, Input, InputRightElement } from '@chakra-ui/react';
+import { ChangeEvent } from 'react';
 
-const NicknameInput = () => (
-  <Flex
-    flexGrow="1"
-    width="100%"
-    paddingRight="19px"
-    paddingLeft="19px"
-    height="70%"
-  >
-    <InputGroup size="md">
-      <Input
-        variant="flushed"
-        size="lg"
-        placeholder="물안경"
-        border="0.7px solid #BFBFBF"
-        borderRadius="10px"
-        paddingLeft="23px"
-      />
-      <InputRightElement paddingRight="16px">
-        <EditIcon width="22px" height="22px" alignSelf="center" />
-      </InputRightElement>
-    </InputGroup>
-  </Flex>
-);
+interface NicknameInputProps {
+  name: string;
+  nickName: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const NicknameInput = ({ name, nickName, onChange }: NicknameInputProps) => {
+  return (
+    <Flex
+      flexGrow="1"
+      width="100%"
+      paddingRight="19px"
+      paddingLeft="19px"
+      height="70%"
+    >
+      <InputGroup size="md">
+        <Input
+          variant="flushed"
+          size="lg"
+          placeholder="물안경"
+          border="0.7px solid #BFBFBF"
+          borderRadius="10px"
+          paddingLeft="23px"
+          name={name}
+          value={nickName}
+          onChange={onChange}
+        />
+        <InputRightElement paddingRight="16px">
+          <EditIcon width="22px" height="22px" alignSelf="center" />
+        </InputRightElement>
+      </InputGroup>
+    </Flex>
+  );
+};
 
 export default NicknameInput;
--- pages/user/[userId]/edit/index.tsx
+++ pages/user/[userId]/edit/index.tsx
@@ -7,15 +7,30 @@ import {
   ProfileImage,
   SubmitButton,
 } from 'components/ProfileEdit';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 const Edit: NextPage = () => {
+  const [nickname, setNickname] = useState('');
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.name);
+    setNickname(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(nickname);
+  }, [nickname]);
+
   return (
     <Flex flexDirection="column" width="100%" height="100%">
       <ProfileEditHeader />
       <Flex width="100%" height="100%" marginTop="48px">
         <Flex width="100%" direction="column" alignItems="center" gap="48px">
           <ProfileImage />
-          <NicknameInput />
+          <NicknameInput
+            name="nickname-input"
+            nickName={nickname}
+            onChange={handleInputChange}
+          />
           <SubmitButton />
         </Flex>
       </Flex>
