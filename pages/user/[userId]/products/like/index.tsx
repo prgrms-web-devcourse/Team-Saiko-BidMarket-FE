5a6beea5d31f5bba4ea174ae467c04e5fab0d2cb
wogha95 <wogha95@naver.com>
2022-07-30 22:24:46 +0900

    ✨ 회원 정보 찜한 상품 페이지 레이아웃 구현

--- pages/user/[userId]/products/like/index.tsx
+++ pages/user/[userId]/products/like/index.tsx
@@ -1,7 +1,28 @@
+import { Center, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
+import { GoBackIcon, Header, SEO } from 'components/common';
+import { NoLikeProducts } from 'components/User';
+
+const DUMMY = [];
+
 const Like: NextPage = () => {
-  return <div>Like</div>;
+  return (
+    <>
+      <SEO title="사용자 이름" />
+      <Header
+        leftContent={<GoBackIcon />}
+        middleContent={<Text>찜한 상품</Text>}
+      />
+      {DUMMY.length === 0 ? (
+        <Center flexDirection="column" height="100%">
+          <NoLikeProducts />
+        </Center>
+      ) : (
+        <Text>list of Product Cards</Text>
+      )}
+    </>
+  );
 };
 
 export default Like;
