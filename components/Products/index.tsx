86489b89af807a5326b713ed51723a7ce3a26dba
wogha95 <wogha95@naver.com>
2022-07-30 20:08:22 +0900

    ✨ 검색결과 페이지 컴포넌트 exports 작성

---
+++ components/Products/index.tsx
@@ -0,0 +1,3 @@
+export { default as SortFilterButton } from './SortFilterButton';
+export { default as CategoryFilterButton } from './CategoryFilterButton';
+export { default as BidFilterCheckBox } from './BidFilterCheckBox';
