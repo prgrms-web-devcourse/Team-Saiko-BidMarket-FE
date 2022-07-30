b5b0ce3b0310590dcf5e4b1a8982d9bb85764b63
Hoseok Na <hsna7024@gmail.com>
2022-07-30 21:35:35 +0900

    :sparkles: SEO Title 추가

--- pages/user/[userId]/edit/index.tsx
+++ pages/user/[userId]/edit/index.tsx
@@ -1,6 +1,7 @@
 import { Flex } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
+import { SEO } from 'components/common';
 import { ProfileEditHeader } from 'components/ProfileEdit';
 import EditProfileForm from 'components/ProfileEdit/EditForm';
 
@@ -11,12 +12,15 @@ const Edit: NextPage = () => {
   };
 
   return (
-    <Flex flexDirection="column" width="100%" height="100%">
-      <ProfileEditHeader />
-      <Flex width="100%" height="100%" marginTop="48px">
-        <EditProfileForm {...dummyProps} />
+    <>
+      <SEO title="프로필 수정" />
+      <Flex flexDirection="column" width="100%" height="100%">
+        <ProfileEditHeader />
+        <Flex width="100%" height="100%" marginTop="48px">
+          <EditProfileForm {...dummyProps} />
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
