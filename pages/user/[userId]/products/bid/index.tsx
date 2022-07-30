77d55556cd616029fe9a8e6a7608fb7d2020fca0
wogha95 <wogha95@naver.com>
2022-07-30 22:13:09 +0900

    ✨ 회원 정보 입찰한 상품 페이지 레이아웃 구현

--- pages/user/[userId]/products/bid/index.tsx
+++ pages/user/[userId]/products/bid/index.tsx
@@ -1,7 +1,28 @@
+import { Center, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
+import { GoBackIcon, Header, SEO } from 'components/common';
+import { NoBidProducts } from 'components/User';
+
+const DUMMY = [];
+
 const Bid: NextPage = () => {
-  return <div>Bid</div>;
+  return (
+    <>
+      <SEO title="사용자 이름" />
+      <Header
+        leftContent={<GoBackIcon />}
+        middleContent={<Text>입찰한 상품</Text>}
+      />
+      {DUMMY.length === 0 ? (
+        <Center flexDirection="column" height="100%">
+          <NoBidProducts />
+        </Center>
+      ) : (
+        <Text>list of Product Cards</Text>
+      )}
+    </>
+  );
 };
 
 export default Bid;
