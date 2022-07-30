edd2a828013a5a194e6b37f619437025e031fc0e
wogha95 <wogha95@naver.com>
2022-07-30 20:54:35 +0900

    ✨ 회원 정보 판매한 상품 페이지 레이아웃 구현

--- pages/user/[userId]/products/sell/index.tsx
+++ pages/user/[userId]/products/sell/index.tsx
@@ -1,7 +1,28 @@
+import { Center, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
+import { GoBackIcon, Header, SEO } from 'components/common';
+import { NoSellProduct } from 'components/User';
+
+const DUMMY = [];
+
 const Sell: NextPage = () => {
-  return <div>Sell</div>;
+  return (
+    <>
+      <SEO title="사용자 이름" />
+      <Header
+        leftContent={<GoBackIcon />}
+        middleContent={<Text>판매한 상품</Text>}
+      />
+      {DUMMY.length === 0 ? (
+        <Center flexDirection="column" height="100%">
+          <NoSellProduct />
+        </Center>
+      ) : (
+        <Text>list of Product Cards</Text>
+      )}
+    </>
+  );
 };
 
 export default Sell;
