598e8af856f1ace3ea254ebd127b293736537ea9
wogha95 <wogha95@naver.com>
2022-07-30 22:13:42 +0900

    🎨 회원 정보 판매한 상품 페이지 TODO 작성

--- pages/user/[userId]/products/bid/index.tsx
+++ pages/user/[userId]/products/bid/index.tsx
@@ -4,11 +4,13 @@ import type { NextPage } from 'next';
 import { GoBackIcon, Header, SEO } from 'components/common';
 import { NoBidProducts } from 'components/User';
 
+// @ TODO 데이터 가져와서 연결 작업
 const DUMMY = [];
 
 const Bid: NextPage = () => {
   return (
     <>
+      {/* @ TODO 실제 사용자 닉네임으로 교체 예정 */}
       <SEO title="사용자 이름" />
       <Header
         leftContent={<GoBackIcon />}
