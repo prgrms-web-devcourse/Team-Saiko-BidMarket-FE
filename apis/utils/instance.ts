a59ee75543528773695ce61dc6d67b7608b71080
waterglasses <hsk10271@gmail.com>
2022-07-31 15:39:10 +0900

    :sparkles: 인증여부, 배포 URL 따른 axios 인스턴스 설정 변경

--- apis/utils/instance.ts
+++ apis/utils/instance.ts
@@ -2,27 +2,31 @@ import axios, { AxiosInstance } from 'axios';
 
 import { interceptors } from './interceptors';
 
-const { REACT_APP_API_END_POINT } = process.env;
+const REACT_APP_API_URL = `${process.env.REACT_APP_API_END_POINT}/api/v1`;
 
-export const baseInstance = (url: string, options: object): AxiosInstance => {
-  return axios.create({
-    baseURL: REACT_APP_API_END_POINT,
+const createBaseInstance = (url: string, options: object): AxiosInstance => {
+  const instance = axios.create({
+    baseURL: url,
     headers: {
       'Content-Type': 'application/json',
     },
     ...options,
   });
+
+  return instance;
 };
 
-export const authInstance = (url: string, options: object): AxiosInstance => {
+const createAuthInstance = (url: string, options: object): AxiosInstance => {
   const instance = axios.create({
-    baseURL: REACT_APP_API_END_POINT,
+    baseURL: url,
     headers: {
       'Content-Type': 'application/json',
     },
     ...options,
   });
-
   interceptors(instance);
   return instance;
 };
+
+export const baseInstance = createBaseInstance(REACT_APP_API_URL, {});
+export const authInstance = createAuthInstance(REACT_APP_API_URL, {});
