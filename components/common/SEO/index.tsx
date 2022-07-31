155abdd90c0c6986283d34069402a9bd4a44b91b
waterglasses <hsk10271@gmail.com>
2022-08-01 01:37:29 +0900

    :sparkle: SEO description과 open graph 항목 추가

--- components/common/SEO/index.tsx
+++ components/common/SEO/index.tsx
@@ -2,12 +2,24 @@ import Head from 'next/head';
 
 interface SEOProps {
   title: string;
+  description?: string;
+  image?: string;
 }
 
-const SEO = ({ title }: SEOProps) => {
+const SEO = ({ title, description, image }: SEOProps) => {
   return (
     <Head>
-      <title>{title} | Bidmarket</title>
+      <title>{title} | BidMarket</title>
+      <meta
+        name="description"
+        content={description || '비딩을 통한 현명한 중고 거래'}
+      />
+      <meta property="og:title" content={title || '비드마켓'} />
+      <meta
+        property="og:description"
+        content={description || '비딩을 통한 현명한 중고 거래'}
+      />
+      <meta property="og:image" content={image || '/svg/bidMarket.svg'} />
     </Head>
   );
 };
