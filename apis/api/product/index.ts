f4d2e4591d3057b6505fe838f84d1aa13b7e164d
waterglasses <hsk10271@gmail.com>
2022-08-01 18:29:04 +0900

    :art: productAPI에서 getProduct 중괄호 제거

--- apis/api/product/index.ts
+++ apis/api/product/index.ts
@@ -1,9 +1,7 @@
 import { baseInstance } from 'apis/utils/instance';
 
 const productAPI = {
-  getProduct: (productId: number) => {
-    baseInstance.get(`/products/${productId}`);
-  },
+  getProduct: (productId: number) => baseInstance.get(`/products/${productId}`),
 };
 
 export default productAPI;
