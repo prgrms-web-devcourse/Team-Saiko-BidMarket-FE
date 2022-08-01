c47f7666ca1c11275565d3d787985b4a47f14393
waterglasses <hsk10271@gmail.com>
2022-08-01 22:15:37 +0900

    :lipstick: product API 경로 수정

--- apis/api/product/index.ts
+++ apis/api/product/index.ts
@@ -1,4 +1,4 @@
-import { baseInstance } from 'apis/utils/instance';
+import { baseInstance } from 'apis/utils/baseInstance';
 
 const productAPI = {
   getProduct: (productId: number) => baseInstance.get(`/products/${productId}`),
