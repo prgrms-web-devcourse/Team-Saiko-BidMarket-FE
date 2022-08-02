9ca1a56a16098e92cd18f6cc28fe35907fd2ccbd
waterglasses <hsk10271@gmail.com>
2022-08-02 15:02:31 +0900

    :sparkles: 상품 상세 판매자 데이터 구현 및 tsx로 확장자 변경

--- components/ProductDetail/ProductSeller.tsx
+++ components/ProductDetail/ProductSeller.tsx
@@ -1,11 +1,16 @@
 import { Flex, Image, Text } from '@chakra-ui/react';
 
-const SellerInfo = () => {
+interface ProductSeller {
+  name: string;
+  profileImageUrl: string;
+}
+
+const ProductSeller = ({ name, profileImageUrl }: ProductSeller) => {
   return (
     <Flex alignItems="center">
       <Image
         alt="profile-image"
-        src="https://bit.ly/code-beast"
+        src={profileImageUrl}
         w="45px"
         h="45px"
         borderRadius="50%"
@@ -15,10 +20,10 @@ const SellerInfo = () => {
         borderColor="brand.primary-900"
       />
       <Text fontWeight="bold" marginLeft="14px">
-        워터
+        {name}
       </Text>
     </Flex>
   );
 };
 
-export default SellerInfo;
+export default ProductSeller;
