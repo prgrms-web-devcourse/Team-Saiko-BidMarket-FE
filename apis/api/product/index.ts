9759ac87be05e3845a01a25910496d7cb56ce329
wogha95 <wogha95@naver.com>
2022-08-02 16:37:42 +0900

    ✨ getProducts API 구현

--- apis/api/product/index.ts
+++ apis/api/product/index.ts
@@ -1,6 +1,16 @@
 import { baseInstance } from 'apis/utils/baseInstance';
+import { ProductsResponseType } from 'types/product';
+
+interface productsAPITypes {
+  offset: number;
+  limit: number;
+}
 
 const productAPI = {
+  getProducts: ({ offset, limit }: productsAPITypes) =>
+    baseInstance.get<ProductsResponseType>(
+      `/products?offset=${offset}&limit=${limit}`
+    ),
   getProduct: (productId: number) => baseInstance.get(`/products/${productId}`),
 };
 
