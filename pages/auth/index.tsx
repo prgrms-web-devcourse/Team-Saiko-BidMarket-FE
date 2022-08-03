79faa1c0bb8d68c1fb591b5311b69dfbe009b500
wogha95 <wogha95@naver.com>
2022-08-03 21:16:19 +0900

    [BM-54] ✨ token 받는 페이지 구현

---
+++ pages/auth/index.tsx
@@ -0,0 +1,23 @@
+import { Center, Spinner } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+import { setItem } from 'apis/utils/storage';
+
+const Auth = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(router);
+    setItem('token', router.query.token as string);
+    router.push('/');
+  }, []);
+  return (
+    <>
+      <Center height="100%">
+        <Spinner size="xl" />
+      </Center>
+    </>
+  );
+};
+export default Auth;
