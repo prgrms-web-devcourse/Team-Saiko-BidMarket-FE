5f545fe15a94739df5a7dac1ce5ee0555643daee
waterglasses <hsk10271@gmail.com>
2022-08-04 02:36:54 +0900

    [BM-200] :sparkles: 잘못 변경한 밝은 회색 기본 색으로 지정 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ const theme = extendTheme({
       'primary-500': '#FFDEE6',
       'primary-100': '#FFECF2',
       dark: '#2E2E2E',
-      'dark-light': 'brand.dark-light',
+      'dark-light': '#8E8E8E',
     },
   },
 });
