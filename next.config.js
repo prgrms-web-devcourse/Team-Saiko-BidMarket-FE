36d687edd2b450d53d79e0a34176cf02fbeca50d
wogha95 <wogha95@naver.com>
2022-08-04 17:41:31 +0900

    [BM-120] 🔧 next.config baseUrl 수정

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   publicRuntimeConfig: {
-    customBaseUrl: `${process.env.NEXT_APP_API_END_POINT}/api/v1`,
+    customBaseUrl: `${process.env.NEXT_APP_API_END_POINT}`,
   },
 };
 
