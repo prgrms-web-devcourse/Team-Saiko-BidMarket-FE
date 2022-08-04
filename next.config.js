a43144ddf61d51f1bfb62ad1ddc6257213af6595
wogha95 <wogha95@naver.com>
2022-08-04 17:25:28 +0900

    [BM-120] 🔧 next.config BaseUrl 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  publicRuntimeConfig: {
+    customBaseUrl: `${process.env.NEXT_APP_API_END_POINT}/api/v1`,
+  },
 };
 
 module.exports = nextConfig;
