6669587ff8f062ea22cfa42e6bcbeba993912303
wogha95 <wogha95@naver.com>
2022-08-04 21:17:32 +0900

    [BM-120] ✨ 메인페이지 데이터 불러오는 API 연동

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { Box, Divider, Flex } from '@chakra-ui/react';
+import { DownloadIcon } from '@chakra-ui/icons';
+import { Box, Button, Divider, Flex } from '@chakra-ui/react';
 import type { InferGetServerSidePropsType } from 'next';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import { productAPI } from 'apis';
 import { Card, SearchInput, SEO } from 'components/common';
@@ -8,12 +9,27 @@ import { Banner, MainHeader, ProductAddButton } from 'components/Main';
 
 export const getServerSideProps = async () => {
   const { data } = await productAPI.getProducts({ offset: 0, limit: 5 });
-  return { props: { products: data } };
+  return { props: { productsProp: data } };
 };
 
 const Home = ({
-  products,
+  productsProp,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const [products, setProducts] = useState(productsProp);
+  let offset = 0;
+  const limit = 5;
+
+  const handleMoreProductClick = async () => {
+    offset = offset + 5;
+    try {
+      const { data } = await productAPI.getProducts({ offset, limit });
+      setProducts([...products, ...data]);
+    } catch (error) {
+      offset = offset - 5;
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <SEO title="비드마켓" />
@@ -30,6 +46,16 @@ const Home = ({
             </Fragment>
           );
         })}
+        <Button
+          alignSelf="center"
+          w="100px"
+          marginTop="20px"
+          borderRadius="30px"
+          color="white"
+          backgroundColor="brand.primary-900"
+        >
+          <DownloadIcon w="5" h="5" onClick={() => handleMoreProductClick()} />
+        </Button>
       </Flex>
       <Box alignSelf="flex-end" position="sticky" bottom="15px" right="15px">
         <ProductAddButton />
