9d68e715fd1e3e923029ed4d61408eba970fc353
wogha95 <wogha95@naver.com>
2022-08-06 22:30:54 +0900

    [BM-139] 🎨 필터 함수 export 작성

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,4 @@
 export { default as priceFormat } from './priceFormat';
 export { default as remainedTimeFormat } from './remainedTimeFormat';
+export { default as categoryOption } from './transformCategoryOptions';
+export { default as sortOption } from './transformSortOptions';
