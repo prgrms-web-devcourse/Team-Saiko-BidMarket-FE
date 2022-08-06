cddf279e7d23bdcb3c5d2e45ed013f29e5cab439
wogha95 <wogha95@naver.com>
2022-08-06 22:34:53 +0900

    [BM-139] ✨ 검색 페이지 카테고리,정렬 필터 컴포넌트 url파싱및 SSR하여 작업

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,30 +1,52 @@
 import { Divider, Flex, Text } from '@chakra-ui/react';
-import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from 'next';
 import { useRouter } from 'next/router';
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 
+import { productAPI } from 'apis';
 import { Card, GoBackIcon, Header, SearchInput, SEO } from 'components/common';
 import { BidFilterCheckBox, FilterButton } from 'components/Products';
-import { CardProductData } from 'types/product';
+import { ProductsResponseType } from 'types/product';
 
-const DUMMY_DATA: Array<CardProductData> = [];
-
+let offset = 0;
+let limit = 10;
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  console.log(context.query);
-  return { props: { queryDatas: context.query } };
+  const { data } = await productAPI.getProducts({ offset: 0, limit: 5 });
+  return { props: { queryDatas: context.query, productsProps: data } };
 };
 
-const Products = ({
+const Products: NextPage = ({
   queryDatas,
+  productsProps,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
-  const [keyword, setKeyword] = useState(queryDatas.title);
-  const [products, setProducts] = useState(DUMMY_DATA);
+  const [title, setTitle] = useState(queryDatas.title);
+  const [selectedSortOption, setSelectedSortOption] = useState(queryDatas.sort);
+  const [selectedCategoryOption, setSelectedCategoryOption] = useState(
+    queryDatas.category
+  );
+  const [progressed, setProgressed] = useState(queryDatas.progressed);
+  const [products, setProducts] = useState<ProductsResponseType>(productsProps);
+  offset = queryDatas.offset;
+  limit = queryDatas.limit;
 
   const handleFormSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    router.push(`products?title=${keyword}&progressed=true&offset=0&limit=10`);
+    // const categoryQuery = category
+    router.push(
+      `/products?title=${title}&sort=${selectedSortOption}&category=${selectedCategoryOption}&progressed=true&offset=0&limit=10`
+    );
   };
+
+  useEffect(() => {
+    router.push(
+      `/products?title=${title}&sort=${selectedSortOption}&category=${selectedCategoryOption}&progressed=true&offset=0&limit=10`
+    );
+  }, [selectedSortOption, selectedCategoryOption]);
 
   return (
     <>
@@ -32,12 +54,20 @@ const Products = ({
       <Header leftContent={<GoBackIcon />} middleContent={<Text>검색</Text>} />
       <Flex direction="column" w="100%">
         <form onSubmit={(event) => handleFormSubmit(event)}>
-          <SearchInput keyword={keyword} onChange={setKeyword} />
+          <SearchInput keyword={title} onChange={setTitle} />
         </form>
         <Divider marginTop="18px" />
         <Flex width="100%" gap="18px" marginTop="15px">
-          <FilterButton filterName="sortFilter" />
-          <FilterButton filterName="categoryFilter" />
+          <FilterButton
+            filterName="sortFilter"
+            selectedSortOption={selectedSortOption}
+            handleFilterChange={setSelectedSortOption}
+          />
+          <FilterButton
+            filterName="categoryFilter"
+            selectedCategoryOption={selectedCategoryOption}
+            handleFilterChange={setSelectedCategoryOption}
+          />
         </Flex>
         <Flex width="100%" marginTop="15px">
           <BidFilterCheckBox />
