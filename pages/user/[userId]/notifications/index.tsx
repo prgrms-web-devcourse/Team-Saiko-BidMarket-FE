aa3dfcc2a54cc46a6868e30a878b88d43099d430
waterglasses <hsk10271@gmail.com>
2022-08-07 23:36:00 +0900

    [BM-236] :sparkles: 알림 유무에 따른 알림 페이지 레이아웃 구현

--- pages/user/[userId]/notifications/index.tsx
+++ pages/user/[userId]/notifications/index.tsx
@@ -1,7 +1,30 @@
+import { Center, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
+import { GoBackIcon, Header } from 'components/common';
+import NoNotifications from 'components/User/NoNotifications';
+
+//TODO : 데이터 연결
+const DUMMY = [];
+
 const Notifications: NextPage = () => {
-  return <div>Notifications</div>;
+  //TODO 인증된 사용자만 페이지 보이도록 구현
+
+  return (
+    <>
+      <Header
+        leftContent={<GoBackIcon />}
+        middleContent={<Text>알림</Text>}
+      />
+      {DUMMY.length === 0 ? (
+        <Center flexDirection="column" height="100%">
+          <NoNotifications />
+        </Center>
+      ) : (
+        <
+      )}
+    </>
+  );
 };
 
 export default Notifications;
