7ff4ea0614f774a044c58386f68d086aa22244e9
waterglasses <hsk10271@gmail.com>
2022-08-08 00:52:18 +0900

    [BM-236] :sparkles: 알림 페이지 레이아웃 구현

--- components/User/Notification.tsx
+++ components/User/Notification.tsx
@@ -1,4 +1,6 @@
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+
+import distanceTimeFormat from 'utils/format/distanceTimeFormat';
 
 interface NotificationProps {
   title: string;
@@ -16,8 +18,33 @@ const Notification = ({
   createdAt,
 }: NotificationProps) => {
   return (
-    <Box>
-      <Image src={iconImage} alt="icon-image" />
+    <Box cursor="pointer" _hover={{ bg: '#FFF6F7' }}>
+      <Flex width="100%" padding="15px 0">
+        <Image src={iconImage} alt="icon-image" w="44px" h="44px" />
+        <Flex width="100%" direction="column" paddingLeft="10px">
+          <Text fontWeight="bold">{title}</Text>
+          <Flex>
+            <Flex direction="column">
+              <Text width="100%" paddingTop="5px">
+                {description}
+              </Text>{' '}
+              <Text color="brand.dark-light">
+                {distanceTimeFormat(createdAt)}
+              </Text>
+            </Flex>
+            <Image
+              w="71px"
+              h="71px"
+              minWidth="71px"
+              src={productImage}
+              alt="product-image"
+              borderRadius="7px"
+              objectFit="cover"
+              marginLeft="15px"
+            />
+          </Flex>
+        </Flex>
+      </Flex>
     </Box>
   );
 };
