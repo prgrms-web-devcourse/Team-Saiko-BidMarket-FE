c8cf271e2ff0fd3df49d4f38e76b98e9a28e99b0
waterglasses <hsk10271@gmail.com>
2022-08-07 16:23:30 +0900

    [BM-158] :sparkles: 입찰가격 유효성 검증 파일 Export

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,4 @@
 export { default as priceFormat } from './format/priceFormat';
 export { default as remainedTimeFormat } from './format/remainedTimeFormat';
+
+export { default as biddingPriceValidation } from './validation/biddingPriceValidation';
