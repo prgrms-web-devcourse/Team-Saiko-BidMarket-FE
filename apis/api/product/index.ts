3fbaf8eef93a829755c4977e85238a17b7aa44e1
wogha95 <wogha95@naver.com>
2022-08-09 08:57:34 +0900

    [BM-286] ♻️ getProducts의 limit 고정, 매개변수 타입 재작성

--- apis/api/product/index.ts
+++ apis/api/product/index.ts
@@ -1,15 +1,10 @@
 import { baseInstance } from 'apis/utils/baseInstance';
 import { ProductResponse, ProductsResponseType } from 'types/product';
 
-interface ProductsAPITypes {
-  offset: number;
-  limit: number;
-}
-
 const productAPI = {
-  getProducts: ({ offset, limit }: ProductsAPITypes) =>
+  getProducts: ({ offset }: { offset: number }) =>
     baseInstance.get<ProductsResponseType>(
-      `/products?offset=${offset}&limit=${limit}`
+      `/products?offset=${offset}&limit=10`
     ),
   getProduct: (productId: number) =>
     baseInstance.get<ProductResponse>(`/products/${productId}`),
