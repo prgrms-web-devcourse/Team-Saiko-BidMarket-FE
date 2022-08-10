ccd8cc2a255b38c89d03baf84abe34ba4353acda
wogha95 <wogha95@naver.com>
2022-08-10 21:33:48 +0900

    [BM-244] 🎨 AddProductCategory export 작성

--- components/CreateProduct/index.tsx
+++ components/CreateProduct/index.tsx
@@ -3,5 +3,6 @@ export { default as AddProductImageUpload } from './AddProductImageUpload';
 export { default as AddProductMinimumPrice } from './AddProductMinimumPrice';
 export { default as AddProductLocation } from './AddProductLocation';
 export { default as AddProductDescription } from './AddProductDescription';
+export { default as AddProductCategory } from './AddProductCategory';
 export { default as SubmitButton } from './SubmitButton';
 export { default as ProductLabel } from './ProductLabel';
