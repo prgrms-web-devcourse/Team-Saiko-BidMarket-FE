f6b70d75a2c5870a7ee0b5dc3f788be699d528c0
wogha95 <wogha95@naver.com>
2022-08-11 07:39:33 +0900

    [BM-297] ✨ getProductsByKeyword API 작성

--- apis/api/product/index.ts
+++ apis/api/product/index.ts
@@ -1,5 +1,7 @@
 import { baseInstance } from 'apis/utils/baseInstance';
+import { categoryOptionsENType } from 'types/categoryOption';
 import { ProductResponse, ProductsResponseType } from 'types/product';
+import { sortOptionsENType } from 'types/sortOption';
 
 const productAPI = {
   getProducts: ({ offset }: { offset: number }) =>
@@ -8,6 +10,22 @@ const productAPI = {
     ),
   getProduct: (productId: number) =>
     baseInstance.get<ProductResponse>(`/products/${productId}`),
+  getProductsByKeyword: ({
+    offset,
+    title,
+    progressed,
+    category,
+    sort,
+  }: {
+    offset: number;
+    title: string;
+    progressed: boolean;
+    category: categoryOptionsENType;
+    sort: sortOptionsENType;
+  }) =>
+    baseInstance.get<ProductsResponseType>(
+      `/products?title=${title}&progressed=${progressed}&category=${category}&sort=${sort}&offset=${offset}&limit=10`
+    ),
 };
 
 export default productAPI;
