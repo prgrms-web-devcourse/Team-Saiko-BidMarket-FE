667c56cef4e07c58d13759b855e131ad78c105df
wogha95 <wogha95@naver.com>
2022-08-11 07:40:01 +0900

    [BM-297] ✨ useGetProductsByKeyword 작성

---
+++ hooks/queries/product/useGetProductsByKeyword.ts
@@ -0,0 +1,58 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+import { productAPI } from 'apis';
+import { categoryOptionsENType } from 'types/categoryOption';
+import { sortOptionsENType } from 'types/sortOption';
+
+const LIMIT = 10;
+
+const getProductsAPI = async ({
+  pageParam = 0,
+  title,
+  progressed,
+  category,
+  sort,
+}: {
+  pageParam: number;
+  title: string;
+  progressed: boolean;
+  category: categoryOptionsENType;
+  sort: sortOptionsENType;
+}) => {
+  const { data } = await productAPI.getProductsByKeyword({
+    offset: pageParam,
+    title,
+    progressed,
+    category,
+    sort,
+  });
+
+  return {
+    data,
+    currentPage: pageParam,
+    isLast: data.length ? false : true,
+  };
+};
+
+const useGetProductsByKeyword = (searchContent: {
+  title: string;
+  progressed: boolean;
+  category: categoryOptionsENType;
+  sort: sortOptionsENType;
+}) => {
+  return useInfiniteQuery(
+    ['products'],
+    ({ pageParam = 0 }) => getProductsAPI({ ...searchContent, pageParam }),
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.isLast) {
+          return undefined;
+        }
+
+        return lastPage.currentPage + LIMIT;
+      },
+    }
+  );
+};
+
+export default useGetProductsByKeyword;
