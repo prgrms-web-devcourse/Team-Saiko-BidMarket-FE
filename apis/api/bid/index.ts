dd53b19199d945de3ead1a5ab90c51553b63480c
waterglasses <hsk10271@gmail.com>
2022-08-11 11:35:22 +0900

    [BM-270] :sparkles: 입찰 금액 조회 API 구현

--- apis/api/bid/index.ts
+++ apis/api/bid/index.ts
@@ -3,6 +3,8 @@ import { authInstance } from 'apis/utils/authInstance';
 const bidAPI = {
   createBid: (productId: number, biddingPrice: number) =>
     authInstance.post(`/biddings`, { productId, biddingPrice }),
+  getBiddingPrice: (productId: number) =>
+    authInstance.get(`/biddngs/products/${productId}`),
 };
 
 export default bidAPI;
