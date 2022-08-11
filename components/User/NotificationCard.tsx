9c42657949a230eb9bec8dd5de2e1172bec9f67f
wogha95 <wogha95@naver.com>
2022-08-11 21:35:33 +0900

    [BM-305] ✨ NotificationCard API 연동

--- components/User/NotificationCard.tsx
+++ components/User/NotificationCard.tsx
@@ -1,11 +1,12 @@
 import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 import distanceTimeFormat from 'utils/format/distanceTimeFormat';
 
 interface NotificationProps {
   title: string;
   description: string;
-  iconImage: string;
+  productId: number;
   productImage: string;
   createdAt: Date;
 }
@@ -13,23 +14,35 @@ interface NotificationProps {
 const Notification = ({
   title,
   description,
-  iconImage,
+  productId,
   productImage,
   createdAt,
 }: NotificationProps) => {
+  const router = useRouter();
+
   return (
-    <Box cursor="pointer" _hover={{ bg: '#FFF6F7' }}>
+    <Box
+      cursor="pointer"
+      w="100%"
+      _hover={{ bg: '#FFF6F7' }}
+      onClick={() => router.push(`/products/${productId}`)}
+    >
       <Flex width="100%" padding="15px 0">
-        <Image src={iconImage} alt="icon-image" w="44px" h="44px" />
+        <Image
+          src="/svg/bidProductMenuIcon.svg"
+          alt="notification-icon"
+          w="44px"
+          h="44px"
+        />
         <Flex width="100%" direction="column" paddingLeft="10px">
           <Text fontWeight="bold">{title}</Text>
           <Flex>
-            <Flex direction="column">
+            <Flex direction="column" flexGrow="1">
               <Text width="100%" paddingTop="5px">
                 {description}
               </Text>
               <Text color="brand.dark-light">
-                {distanceTimeFormat(createdAt)}
+                {distanceTimeFormat(new Date(createdAt))}
               </Text>
             </Flex>
             <Image
