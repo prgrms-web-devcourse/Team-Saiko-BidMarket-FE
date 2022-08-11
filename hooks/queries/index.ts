88bff07332c3cf4fbbf4f3ca5454b5425f9a6bc4
wogha95 <wogha95@naver.com>
2022-08-11 22:33:05 +0900

    [BM-305] 🎨 useGetNotifications export 작성

--- hooks/queries/index.ts
+++ hooks/queries/index.ts
@@ -1,3 +1,4 @@
+import useGetNotifications from './notification/useGetNotifications';
 import useGetProducts from './product/useGetProducts';
 
-export { useGetProducts };
+export { useGetProducts, useGetNotifications };
