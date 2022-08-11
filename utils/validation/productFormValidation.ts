d4f23bc323e3998f05e58830f6beebdc9ca96e32
taewook1 <aktw96@naver.com>
2022-08-12 04:24:46 +0900

    [BM-317] ✨ product validation 부분utils/validation으로 컴포넌트화

---
+++ utils/validation/productFormValidation.ts
@@ -0,0 +1,59 @@
+interface productFormProps {
+  title: string;
+  minimumPrice: number;
+  location: string;
+  images: string;
+  category: string;
+  description: string;
+}
+
+const productFormValidation = ({
+  title,
+  minimumPrice,
+  location,
+  images,
+  category,
+  description,
+}: productFormProps) => {
+  const error: {
+    title?: string;
+    minimumPrice?: string;
+    location?: string;
+    category?: string;
+    images?: string;
+    description?: string;
+  } = {};
+  if (!title.trim()) {
+    error.title = '상품 제목을 입력해주세요.';
+  }
+
+  // if (!images) {
+  //   error.images = '상품 이미지를 넣어주세요.';
+  // }
+
+  // if (minimumPrice && parseInt(minimumPrice, 10) < 1000) {
+  //   error.minimumPrice = '1000원 이상 입력 가능합니다.';
+  // }
+  // else if (minimumPrice && Math.floor(minimumPrice / 100) * 100) {
+  //   error.minimumPrice = '100원 단위로 입력 가능합니다.';
+  // }
+
+  // if (productImageArray.length === 0) {
+  //   error.images = '사진 1개 이상 필요합니다.';
+  // }
+
+  if (!location.trim()) {
+    error.location = '희망 거래지역을 입력해주세요.';
+  }
+
+  // if (category === '') {
+  //   error.category = '카테고리를 선택해주세요.';
+  // }
+  if (!description.trim()) {
+    error.description = '상세 내용을 입력해주세요.';
+  }
+
+  return error;
+};
+
+export default productFormValidation;
