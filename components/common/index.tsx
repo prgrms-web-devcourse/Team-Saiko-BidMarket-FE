a4dae94986dafbee5d6829dfdd509ecb85762621
waterglasses <hsk10271@gmail.com>
2022-08-12 15:38:25 +0900

    [BM-328] :sparkles: 헤더 타이틀 컴포넌트 구현

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -4,6 +4,7 @@ export { default as ProductCardContainer } from './ProductCardContainer';
 export { default as Header } from './Header';
 export { default as GoBackIcon } from './Header/GoBackIcon';
 export { default as SideBar } from './Header/SideBar';
+export { default as HeaderTitle } from './Header/HeaderTitle';
 
 export { default as SearchInput } from './SearchInput';
 
