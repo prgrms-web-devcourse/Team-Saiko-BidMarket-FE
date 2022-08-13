5a8dbe7a2fa0fd21a0972bb81ccc0ce2742aa4f9
wogha95 <wogha95@naver.com>
2022-08-13 19:34:14 +0900

    [BM-341] ✨ 회원 정보 페이지 계정 설정 페이지 연결 및 UI 반영

--- components/User/UserSetting.tsx
+++ components/User/UserSetting.tsx
@@ -1,35 +1,38 @@
-import { Flex, Img, Text } from '@chakra-ui/react';
+import { ChevronRightIcon, SettingsIcon } from '@chakra-ui/icons';
+import { Flex, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 
-import { removeItem } from 'apis/utils/storage';
+interface UserSettingProps {
+  userId: number;
+}
 
-const UserSetting = () => {
+const UserSetting = ({ userId }: UserSettingProps) => {
   const router = useRouter();
 
   return (
     <Flex flexDirection="column" width="100%" marginTop="21px" gap="19px">
-      <Text
-        textAlign="left"
-        color="brand.dark"
-        fontFamily="Roboto"
-        fontStyle="normal"
-        fontWeight="700"
-        fontSize="16px"
-        lineHeight="128.19%"
-      >
-        계정 설정
-      </Text>
       <Flex
         alignItems="center"
+        justifyContent="space-between"
         gap="16px"
         cursor="pointer"
         onClick={() => {
-          removeItem('token');
-          router.push('/');
+          router.push(`/user/${userId}/setting`);
         }}
       >
-        <Img src="/svg/logout.svg" />
-        <Text>로그아웃</Text>
+        <Flex alignItems="center" gap="10px">
+          <SettingsIcon />
+          <Text
+            textAlign="left"
+            color="brand.dark"
+            fontFamily="Roboto"
+            fontStyle="normal"
+            fontSize="16px"
+          >
+            계정 설정
+          </Text>
+        </Flex>
+        <ChevronRightIcon />
       </Flex>
     </Flex>
   );
