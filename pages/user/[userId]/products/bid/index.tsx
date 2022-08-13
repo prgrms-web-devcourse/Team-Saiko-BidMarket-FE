f73f721295a4bd4dac34a73d52bc8acb8b81a986
wogha95 <wogha95@naver.com>
2022-08-14 08:26:50 +0900

    [BM-276] ✨ 회원 정보 입찰한 상품 페이지 본인 확인 작업 구현

--- pages/user/[userId]/products/bid/index.tsx
+++ pages/user/[userId]/products/bid/index.tsx
@@ -1,14 +1,18 @@
-import { DownloadIcon } from '@chakra-ui/icons';
-import { Button, Center, Divider, Spinner } from '@chakra-ui/react';
-import type { NextPage } from 'next';
-import { Fragment, useEffect } from 'react';
+import { Center, Spinner } from '@chakra-ui/react';
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
+import { userAPI } from 'apis';
 import {
   GoBackIcon,
   Header,
   HeaderTitle,
-  ProductCard,
   ProductCardContainer,
   SEO,
 } from 'components/common';
@@ -16,8 +20,29 @@ import { NoProducts } from 'components/User';
 import { useGetUserBidProducts } from 'hooks/queries';
 import useLoginUser from 'hooks/useLoginUser';
 
-const Bid: NextPage = () => {
-  const { id: authUserId, username } = useLoginUser();
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { userId } = query;
+  let user = {};
+
+  try {
+    user = (await userAPI.getUser(parseInt(userId as string, 10))).data;
+  } catch (error) {
+    console.error(error);
+  }
+
+  return {
+    props: {
+      user,
+    },
+  };
+};
+
+const Bid: NextPage = ({
+  user: { id, username },
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  const userId = parseInt(id, 10);
+  const { id: authUserId } = useLoginUser();
   const {
     data: productPages,
     fetchNextPage,
@@ -26,8 +51,12 @@ const Bid: NextPage = () => {
   const [ref, isView] = useInView();
 
   useEffect(() => {
-    console.log(authUserId);
     if (authUserId === -1) {
+      return;
+    }
+
+    if (userId !== authUserId) {
+      router.push('/');
       return;
     }
   }, [authUserId]);
@@ -38,7 +67,12 @@ const Bid: NextPage = () => {
     }
   }, [isView, productPages]);
 
-  if (!authUserId) {
+  // @TODO 토큰이 없는 경우 계속 Spinner 렌더링 현상 개선 필요
+  // useLoginUser 내부에 토큰여부에 의해서든 API에 의해서든 업데이트 되었다는 신호가 필요
+  // @TODO 페이지에 진입 후 판단하는 문제 존재
+  // 따라서 찰나의 순간 목록이 렌더링된다 -> 불필요한 작업으로 성능 다운
+  // cf) 토큰은 있지만 다른 회원인 경우 메인페이지로 이동
+  if (authUserId === -1) {
     return (
       <Center height="100%">
         <Spinner size="xl" />
