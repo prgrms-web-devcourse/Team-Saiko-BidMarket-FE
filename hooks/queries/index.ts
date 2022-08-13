ed2946378f11e35bffc633d07621ed5971b325eb
wogha95 <wogha95@naver.com>
2022-08-14 07:09:58 +0900

    [BM-276] 🎨 getSellProducts export 작성

--- hooks/queries/index.ts
+++ hooks/queries/index.ts
@@ -1,5 +1,11 @@
 import useGetNotifications from './notification/useGetNotifications';
 import useGetProducts from './product/useGetProducts';
 import useGetProductsByKeyword from './product/useGetProductsByKeyword';
+import useGetUserSellProducts from './user/useGetUserSellProducts';
 
-export { useGetProducts, useGetProductsByKeyword, useGetNotifications };
+export {
+  useGetProducts,
+  useGetProductsByKeyword,
+  useGetNotifications,
+  useGetUserSellProducts,
+};
