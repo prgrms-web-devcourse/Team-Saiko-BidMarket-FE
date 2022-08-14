37de1e5ccba800f05ba4958a65c6b85e0a523950
Hoseok Na <hsna7024@gmail.com>
2022-08-14 18:20:55 +0900

    :lipstick: 보내는 메세지 테두리 및 마진 수정

--- components/ChatRoom/SendingMessage.tsx
+++ components/ChatRoom/SendingMessage.tsx
@@ -9,13 +9,14 @@ interface SendingMessageProps {
 
 const SendingMessage = ({ content, createdAt }: SendingMessageProps) => {
   return (
-    <Flex width="100%" gap="16px" justifyContent="flex-end" alignItems="center">
+    <Flex width="100%" justifyContent="flex-end" alignItems="center">
       <ChatTimeText createdAt={createdAt} />
       <Box
         maxWidth="55%"
         bgColor=" #FF4370"
-        borderRadius="0px 20px 20px 20px"
+        borderRadius="20px 0px 20px 20px"
         padding="12px 16px 12px 16px"
+        marginLeft="4px"
       >
         <Text
           color="white"
