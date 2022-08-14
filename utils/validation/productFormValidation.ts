54f40fcbc65ae6b3bfc4812938fafca27dd7089c
taewook1 <aktw96@naver.com>
2022-08-14 14:25:53 +0900

    [BM-317] ✨ 상품 등록에 대한 검증

--- utils/validation/productFormValidation.ts
+++ utils/validation/productFormValidation.ts
@@ -27,16 +27,20 @@ const productFormValidation = ({
     error.title = '상품 제목을 입력해주세요.';
   }
 
-  // if (!images) {
-  //   error.images = '상품 이미지를 넣어주세요.';
-  // }
+  if (!images) {
+    error.images = '상품 이미지를 넣어주세요.';
+  }
 
-  // if (minimumPrice && parseInt(minimumPrice, 10) < 1000) {
-  //   error.minimumPrice = '1000원 이상 입력 가능합니다.';
-  // }
-  // else if (minimumPrice && Math.floor(minimumPrice / 100) * 100) {
-  //   error.minimumPrice = '100원 단위로 입력 가능합니다.';
-  // }
+  if (minimumPrice === 0) {
+    error.minimumPrice = '금액을 입력해주세요.';
+  }
+  if (minimumPrice && minimumPrice < 1000) {
+    error.minimumPrice = '1000원 이상 입력 가능합니다.';
+  }
+  console.log(minimumPrice % 100);
+  if (minimumPrice && minimumPrice % 100 !== 0) {
+    error.minimumPrice = '100원 단위로 입력 가능합니다.';
+  }
 
   // if (productImageArray.length === 0) {
   //   error.images = '사진 1개 이상 필요합니다.';
@@ -49,6 +53,7 @@ const productFormValidation = ({
   // if (category === '') {
   //   error.category = '카테고리를 선택해주세요.';
   // }
+
   if (!description.trim()) {
     error.description = '상세 내용을 입력해주세요.';
   }
