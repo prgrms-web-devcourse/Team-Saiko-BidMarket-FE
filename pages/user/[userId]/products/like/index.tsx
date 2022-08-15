de610c197eb4216964994c8fc114c7c4f37ffd96
wogha95 <wogha95@naver.com>
2022-08-15 18:22:46 +0900

    [BM-332] ✨ 회원 정보 찜한 상품 페이지 무한 스크롤 적용

--- pages/user/[userId]/products/like/index.tsx
+++ pages/user/[userId]/products/like/index.tsx
@@ -1,27 +1,106 @@
-import { Center, Text } from '@chakra-ui/react';
-import type { NextPage } from 'next';
+import { Center, Spinner, Text } from '@chakra-ui/react';
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
-import { GoBackIcon, Header, HeaderTitle, SEO } from 'components/common';
+import { userAPI } from 'apis';
+import {
+  GoBackIcon,
+  Header,
+  HeaderTitle,
+  ProductCardContainer,
+  SEO,
+} from 'components/common';
 import { NoProducts } from 'components/User';
+import { useGetUserLikeProducts } from 'hooks/queries';
+import useLoginUser from 'hooks/useLoginUser';
 
-// @ TODO 데이터 가져와서 연결 작업
-const DUMMY = [];
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { userId } = query;
+  let user = {};
 
-const Like: NextPage = () => {
+  try {
+    user = (await userAPI.getUser(parseInt(userId as string, 10))).data;
+  } catch (error) {
+    console.error(error);
+  }
+
+  return {
+    props: {
+      user,
+    },
+  };
+};
+
+const Like: NextPage = ({
+  user: { id, username },
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  const userId = parseInt(id, 10);
+  const { id: authUserId } = useLoginUser();
+  const {
+    data: productPages,
+    fetchNextPage,
+    hasNextPage,
+  } = useGetUserLikeProducts();
+  const [ref, isView] = useInView();
+
+  useEffect(() => {
+    if (authUserId === -1) {
+      return;
+    }
+
+    if (userId !== authUserId) {
+      router.push('/');
+      return;
+    }
+  }, [authUserId]);
+
+  useEffect(() => {
+    if (isView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [isView, productPages]);
+
+  if (authUserId === -1) {
+    return (
+      <Center height="100%">
+        <Spinner size="xl" />
+      </Center>
+    );
+  }
+
   return (
     <>
-      {/* @ TODO 실제 사용자 닉네임으로 교체 예정 */}
-      <SEO title="사용자 이름" />
+      <SEO title={`${username}의 판매한 상품`} />
       <Header
         leftContent={<GoBackIcon />}
         middleContent={<HeaderTitle title="찜한 상품" />}
       />
-      {DUMMY.length === 0 ? (
+      {productPages?.pages.map(({ data }, pageIndex) => {
+        return data.map((product, productIndex) => {
+          const lastPageIndex = productPages.pages.length - 1;
+          const lastProductIndex = data.length - 1;
+          const isLastProduct =
+            lastPageIndex === pageIndex && lastProductIndex === productIndex;
+          return isLastProduct ? (
+            <div ref={ref} key={product.id}>
+              <ProductCardContainer product={product} />
+            </div>
+          ) : (
+            <ProductCardContainer key={product.id} product={product} />
+          );
+        });
+      })}
+      {productPages?.pages[0].data.length === 0 && (
         <Center flexDirection="column" height="100%">
           <NoProducts pageName="userLikeProducts" />
         </Center>
-      ) : (
-        <Text>list of Product Cards</Text>
       )}
     </>
   );
