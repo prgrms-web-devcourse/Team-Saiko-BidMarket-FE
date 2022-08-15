af774471ff4513b28802d207997d03fa2dce6443
waterglasses <hsk10271@gmail.com>
2022-08-15 20:43:56 +0900

    [BM-359] :sparkles: 신고하기 변경된 API 반영

--- apis/api/report/index.tsx
+++ apis/api/report/index.tsx
@@ -1,10 +1,8 @@
 import { authInstance } from 'apis/utils/authInstance';
 
 const reportAPI = {
-  createReportByProduct: (productId: number, reason: string) =>
-    authInstance.post(`/reports/products/${productId}`, { reason }),
-  createReportByUser: (userId: number, reason: string) =>
-    authInstance.post(`/reports/users/${userId}`, { reason }),
+  createReport: (type: string, typeId: number, reason: string) =>
+    authInstance.post(`/reports`, { type, typeId, reason }),
 };
 
 export default reportAPI;
