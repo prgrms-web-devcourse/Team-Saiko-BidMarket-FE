3288a88f1ebb41829689447bdab9ffdd31fb9b59
waterglasses <hsk10271@gmail.com>
2022-08-16 01:57:01 +0900

    [BM-353] :sparkles: 상품 등록 최소 금액 10000만원 제한 유효성검사 추가

--- utils/validation/productFormValidation.ts
+++ utils/validation/productFormValidation.ts
@@ -30,14 +30,21 @@ const productFormValidation = ({
   if (images.length === 0) {
     error.images = '상품 이미지를 넣어주세요.';
   }
+
   if (minimumPrice === 0) {
     error.minimumPrice = '금액을 입력해주세요.';
   }
+
   if (minimumPrice && minimumPrice < 1000) {
     error.minimumPrice = '1000원 이상 입력 가능합니다.';
   }
+
   if (minimumPrice && minimumPrice % 100 !== 0) {
     error.minimumPrice = '100원 단위로 입력 가능합니다.';
+  }
+
+  if (minimumPrice > 10000000) {
+    error.minimumPrice = '1000만원 이상 입력할 수 없습니다.';
   }
 
   if (!location.trim()) {
