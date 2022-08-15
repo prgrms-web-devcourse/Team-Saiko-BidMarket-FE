d40dd93871bc12a50dcc6c41bec0a2deb9f8ac41
waterglasses <hsk10271@gmail.com>
2022-08-15 21:31:31 +0900

    [BM-359] :sparkles: 상품 등록 버튼 로그인 안할 시 안보이도록 구현

--- components/Main/ProductAddButton.tsx
+++ components/Main/ProductAddButton.tsx
@@ -1,21 +1,34 @@
 import { AddIcon } from '@chakra-ui/icons';
 import { Button } from '@chakra-ui/react';
+import useLoginUser from 'hooks/useLoginUser';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 const ProductAddButton = () => {
   const router = useRouter();
+  const [isLogin, setIsLogin] = useState(false);
+  const { id: userId, profileImage: profileImageUrl } = useLoginUser();
+
+  // @TODO HOOK으로 개선 (BM-184 참고)
+  useEffect(() => {
+    if (userId !== -1) {
+      setIsLogin(true);
+    }
+  }, [userId]);
 
   return (
-    <Button
-      w="60px"
-      h="60px"
-      borderRadius="50px"
-      backgroundColor="brand.primary-900"
-      aria-label="product-add-button"
-      onClick={() => router.push('/product')}
-    >
-      <AddIcon color="white" />
-    </Button>
+    isLogin && (
+      <Button
+        w="60px"
+        h="60px"
+        borderRadius="50px"
+        backgroundColor="brand.primary-900"
+        aria-label="product-add-button"
+        onClick={() => router.push('/product')}
+      >
+        <AddIcon color="white" />
+      </Button>
+    )
   );
 };
 
