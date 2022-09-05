34c48b0609f1f54086079acd43ac57c1400ec481
Hoseok Na <hsna7024@gmail.com>
2022-09-05 21:23:54 +0900

    :recycle: 주석 제거

--- utils/format/chatDateFormat.ts
+++ utils/format/chatDateFormat.ts
@@ -1,9 +1,6 @@
 import { format } from 'date-fns';
 import { ko } from 'date-fns/locale';
 
-/**
- *  2022년 9월 4일
- */
 const chatDateFormat = (createdAt: Date) =>
   format(new Date(createdAt), 'yyyy년 M월 d일', { locale: ko });
 
