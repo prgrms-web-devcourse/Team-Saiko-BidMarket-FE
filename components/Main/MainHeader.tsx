09adf21b60093356c7e3e172aebf9039886b6f72
Hoseok Na <hsna7024@gmail.com>
2022-09-11 16:15:54 +0900

    :recycle: 컴포넌트가 아닌 메인페이지에서 스피너 하도록 수정

--- components/Main/MainHeader.tsx
+++ components/Main/MainHeader.tsx
@@ -1,28 +1,18 @@
 import { BellIcon, ChatIcon } from '@chakra-ui/icons';
 import { Avatar, Circle, Flex, Image } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 
-import { Header, Loading } from 'components/common';
-import useLoginUser from 'hooks/useLoginUser';
+import { Header } from 'components/common';
+import { User } from 'types/user';
 
 import LoginButton from './LoginButton';
 
-const MainHeader = () => {
-  const router = useRouter();
-  const [isLogin, setIsLogin] = useState(false);
-  const {
-    isAuthFinished,
-    isAuthUser,
-    authUser: { id: userId, profileImage },
-  } = useLoginUser({
-    handleAuthUser: ({ isAuthUser }) => setIsLogin(isAuthUser),
-  });
+interface MainHeaderProps {
+  authUser: User;
+}
 
-  // TODO: 로딩은 메인 페이지에서 처리하도록 수정
-  if (!isAuthFinished || isAuthUser !== isLogin) {
-    return <Loading />;
-  }
+const MainHeader = ({ authUser: { id, profileImage } }: MainHeaderProps) => {
+  const router = useRouter();
 
   return (
     <Header
@@ -35,25 +25,25 @@ const MainHeader = () => {
       }
       // @TODO 컴포넌트로 분리 (BM-184 참고)
       rightContent={
-        isLogin ? (
+        id !== -1 ? (
           <Flex gap="10px" alignItems="center">
             <ChatIcon
               w="24px"
               h="24px"
               _hover={{ cursor: 'pointer' }}
-              onClick={() => router.push(`/user/${userId}/chat`)}
+              onClick={() => router.push(`/user/${id}/chat`)}
             />
             <BellIcon
               w="32px"
               h="32px"
               _hover={{ cursor: 'pointer' }}
-              onClick={() => router.push(`/user/${userId}/notifications`)}
+              onClick={() => router.push(`/user/${id}/notifications`)}
             />
             <Circle
               border="2px solid"
               borderColor="brand.primary-900"
               _hover={{ cursor: 'pointer' }}
-              onClick={() => router.push(`/user/${userId}`)}
+              onClick={() => router.push(`/user/${id}`)}
             >
               <Avatar name="프로필 이미지" size="sm" src={profileImage} />
             </Circle>
