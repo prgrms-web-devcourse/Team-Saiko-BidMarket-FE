a8379f8c8ab79109df0a3de33661598b45ebc6a4
waterglasses <hsk10271@gmail.com>
2022-09-17 17:17:34 +0900

    [BM-389] :recycle: 신고하기 유저/상품 이미지 next/image로 개선

--- components/ReportForm/UserInfo.tsx
+++ components/ReportForm/UserInfo.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Text } from '@chakra-ui/react';
+import { Circle, Flex, Image, Text } from '@chakra-ui/react';
 
 interface UserInfoProps {
   userInfo: {
@@ -12,13 +12,14 @@ const UserInfo = ({ userInfo }: UserInfoProps) => {
 
   return (
     <Flex gap="15px" alignItems="center">
-      <Image
-        w="90px"
-        h="90px"
-        src={profileImage}
-        alt={`${username}-프로필-이미지`}
-        borderRadius="50%"
-      />
+      <Circle borderRadius="7px" overflow="hidden" w="90px" h="90px">
+        <Image
+          width="90px"
+          height="90px"
+          src={profileImage}
+          alt={`${username}-프로필-이미지`}
+        />
+      </Circle>
       <Text fontSize="lg">{username}</Text>
     </Flex>
   );
