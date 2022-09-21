3a03309d4c831932cd2f802994fba75b1551c7d4
waterglasses <hsk10271@gmail.com>
2022-09-21 15:11:17 +0900

    [BM-379] :recycle: 상품 추가 버튼 useRotuer 훅 Link 태그로 개선

--- components/Main/ProductAddButton.tsx
+++ components/Main/ProductAddButton.tsx
@@ -1,31 +1,31 @@
 import { AddIcon } from '@chakra-ui/icons';
 import { Button } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { useState } from 'react';
 
 import useLoginUser from 'hooks/useLoginUser';
 
 const ProductAddButton = () => {
-  const router = useRouter();
   const [isLogin, setIsLogin] = useState(false);
 
   useLoginUser({
     handleAuthUser: ({ isAuthUser }) => setIsLogin(isAuthUser),
   });
 
-  return isLogin ? (
-    <Button
-      w="60px"
-      h="60px"
-      borderRadius="50px"
-      backgroundColor="brand.primary-900"
-      aria-label="product-add-button"
-      onClick={() => router.push('/product')}
-    >
-      <AddIcon color="white" />
-    </Button>
-  ) : (
-    <></>
+  return (
+    isLogin && (
+      <Link href={'/product'}>
+        <Button
+          w="60px"
+          h="60px"
+          borderRadius="50px"
+          backgroundColor="brand.primary-900"
+          aria-label="product-add-button"
+        >
+          <AddIcon color="white" />
+        </Button>
+      </Link>
+    )
   );
 };
 
