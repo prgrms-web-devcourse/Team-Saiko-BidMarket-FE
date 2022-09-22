e3c4cf4d49b8e5bca2d5c824a0baf692451ebbce
waterglasses <hsk10271@gmail.com>
2022-09-22 14:53:04 +0900

    [BM-379] :recycle: Link 태그 a태그 추가 개선

--- components/Main/LoginButton.tsx
+++ components/Main/LoginButton.tsx
@@ -3,16 +3,18 @@ import Link from 'next/link';
 
 const LoginButton = () => {
   return (
-    <Link href={'/login'}>
-      <Button
-        border="1px"
-        borderColor="brand.primary-900"
-        backgroundColor="white"
-        color="brand.primary-900"
-        _hover={{ bg: 'brand.primary-100' }}
-      >
-        로그인
-      </Button>
+    <Link href={'/login'} passHref>
+      <a>
+        <Button
+          border="1px"
+          borderColor="brand.primary-900"
+          backgroundColor="white"
+          color="brand.primary-900"
+          _hover={{ bg: 'brand.primary-100' }}
+        >
+          로그인
+        </Button>
+      </a>
     </Link>
   );
 };
