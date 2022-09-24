b1b0bb7dd4f4447d5cc3345c065e3141b46636e9
waterglasses <hsk10271@gmail.com>
2022-09-24 16:44:02 +0900

    [BM-379] :recycle: 유저 계정 설정 useRouter 훅 Link 태그로 개선

--- components/User/UserSetting.tsx
+++ components/User/UserSetting.tsx
@@ -1,39 +1,36 @@
 import { ChevronRightIcon, SettingsIcon } from '@chakra-ui/icons';
 import { Flex, Text } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 interface UserSettingProps {
   userId: number;
 }
 
 const UserSetting = ({ userId }: UserSettingProps) => {
-  const router = useRouter();
-
   return (
     <Flex flexDirection="column" width="100%" marginTop="21px" gap="19px">
-      <Flex
-        alignItems="center"
-        justifyContent="space-between"
-        gap="16px"
-        cursor="pointer"
-        onClick={() => {
-          router.push(`/user/${userId}/setting`);
-        }}
-      >
-        <Flex alignItems="center" gap="10px">
-          <SettingsIcon />
-          <Text
-            textAlign="left"
-            color="brand.dark"
-            fontFamily="Roboto"
-            fontStyle="normal"
-            fontSize="16px"
-          >
-            계정 설정
-          </Text>
+      <Link href={`/user/${userId}/setting`} passHref>
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          gap="16px"
+          cursor="pointer"
+        >
+          <Flex alignItems="center" gap="10px">
+            <SettingsIcon />
+            <Text
+              textAlign="left"
+              color="brand.dark"
+              fontFamily="Roboto"
+              fontStyle="normal"
+              fontSize="16px"
+            >
+              계정 설정
+            </Text>
+          </Flex>
+          <ChevronRightIcon />
         </Flex>
-        <ChevronRightIcon />
-      </Flex>
+      </Link>
     </Flex>
   );
 };
