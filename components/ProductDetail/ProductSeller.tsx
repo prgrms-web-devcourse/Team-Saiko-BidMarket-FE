43211814697da33f45200f9c056aa777e4138f9d
waterglasses <hsk10271@gmail.com>
2022-09-24 16:43:23 +0900

    [BM-379] :recycle: 상품 판매자 userRouter 훅 Link 태그로 개선

--- components/ProductDetail/ProductSeller.tsx
+++ components/ProductDetail/ProductSeller.tsx
@@ -1,6 +1,6 @@
 import { Flex, Box, Text } from '@chakra-ui/react';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 interface ProductSellerProps {
   userId: number;
@@ -9,36 +9,33 @@ interface ProductSellerProps {
 }
 
 const ProductSeller = ({ userId, name, profileImage }: ProductSellerProps) => {
-  const router = useRouter();
-
   return (
-    <Box
-      _hover={{ cursor: 'pointer' }}
-      onClick={() => router.push(`/user/${userId}`)}
-    >
-      <Flex alignItems="center">
-        <Box
-          w="45px"
-          h="45px"
-          borderRadius="50%"
-          marginTop="14px"
-          marginBottom="14px"
-          border="1px"
-          borderColor="brand.primary-900"
-          overflow="hidden"
-        >
-          <Image
-            alt="profile-image"
-            src={profileImage}
-            width="45px"
-            height="45px"
-          />
-        </Box>
-        <Text fontWeight="bold" marginLeft="14px">
-          {name === 'UnKnown' ? '알 수 없음' : name}
-        </Text>
-      </Flex>
-    </Box>
+    <Link href={`/user/${userId}`} passHref>
+      <Box _hover={{ cursor: 'pointer' }}>
+        <Flex alignItems="center">
+          <Box
+            w="45px"
+            h="45px"
+            borderRadius="50%"
+            marginTop="14px"
+            marginBottom="14px"
+            border="1px"
+            borderColor="brand.primary-900"
+            overflow="hidden"
+          >
+            <Image
+              alt="profile-image"
+              src={profileImage}
+              width="45px"
+              height="45px"
+            />
+          </Box>
+          <Text fontWeight="bold" marginLeft="14px">
+            {name === 'UnKnown' ? '알 수 없음' : name}
+          </Text>
+        </Flex>
+      </Box>
+    </Link>
   );
 };
 
