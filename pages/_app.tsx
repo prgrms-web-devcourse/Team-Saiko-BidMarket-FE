e1d867b783cc8cc9f9e4ed0196d67fceb70981fb
waterglasses <hsk10271@gmail.com>
2022-09-30 18:47:11 +0900

    [BM-391] :recycle: App.js에 로딩 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
+import { Router } from 'next/router';
+import { useEffect, useState } from 'react';
 
+import { Loading } from 'components/common';
 import Layout from 'components/Layout';
 
 const theme = extendTheme({
@@ -20,11 +23,32 @@ const theme = extendTheme({
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const startLoading = () => {
+      setIsLoading(true);
+    };
+
+    const finishedLoading = () => {
+      setIsLoading(false);
+
+    Router.events.on('routeChangeStart', startLoading);
+    Router.events.on('routeChangeComplete', finishedLoading);
+    Router.events.on('routeChangeError', finishedLoading);
+
+    return () => {
+      Router.events.off('routeChangeStart', startLoading);
+      Router.events.off('routeChangeComplete', finishedLoading);
+      Router.events.off('routeChangeError', finishedLoading);
+    };
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
         <Layout>
-          <Component {...pageProps} />
+          {isLoading ? <Loading /> : <Component {...pageProps} />}
         </Layout>
       </ChakraProvider>
       <ReactQueryDevtools initialIsOpen={false} />
